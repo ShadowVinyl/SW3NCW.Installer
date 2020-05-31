d1ff75a9e53894951da966127caa2379c226f5ff
AlekseySR <leha.petrachkov@yandex.ru>
2020-05-31 20:27:04 +0700

    Main source code of the project

---
+++ Source/FileClass.h
@@ -0,0 +1,18 @@
+
+class FileClass
+{
+public:
+	static int  FtpGetStatus();
+	static long FileSize(const char* FileName);
+	static bool FileExists(const char* FileName);
+	static double FtpGetFileSize(const char* FileName);
+	static void FileQueueSet(wchar_t* DestDir);
+	static void FilesDelete();
+
+private:
+	static bool FileDownload(const char* FileName);
+	static bool FileOpen(const char* FileName);
+	static int  ShellMoveFiles(const wchar_t* srcPath, const wchar_t* newPath);
+};
+
+std::wstring GetExePath();
