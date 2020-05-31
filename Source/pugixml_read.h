d4902b693b5eac222a7afb789b01651fba65a140
AlekseySR <leha.petrachkov@yandex.ru>
2020-05-31 20:04:53 +0700

    1

--- Source/pugixml_read.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "pugixml.hpp"
-
-wchar_t* GetXMLConfigFileName();
-std::string ReadXMLConfigTag(const char* TagName);
-bool WriteXMLConfigTag(const char* TagName, const char* TagContent);
